b9c6b1c6025195fb95880da09eaf177659fb4506
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-01-25 07:29:14 +0000

    [add] ジャンル一覧 進捗50%

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,37 @@
+class Admin::GenresController < ApplicationController
+  # ジャンルコントローラ
+  # Viewへ渡すためのインスタンス変数
+  def new
+    @genre = Genre.new
+  end
+  
+  def index
+    @genres = Genre.all
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    # リダイレクト (一応：一覧画面へ)
+    redirect_to admin_genres_path
+  end
+
+  def edit
+    @genre= Genre.find(params[:id])
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    # リダイレクト (一応：一覧画面へ)
+    redirect_to admin_genres_path
+  end
+
+  private
+
+   # ストロングパラメータ
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  # 管理者
+  namespace :admin do
+    resources :genres, only: [:index, :new, :create, :show, :edit, :update]
+  end
+  # 顧客
+  namespace :public do
+    resources :genres, only: [:index, :show]
+  end
+
+  namespace :admin do
+    # get 'genres/index'
+    # get 'genres/edit'
+    resources :genres, only: [:new, :index, :create, :edit, :update]
+  end
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
