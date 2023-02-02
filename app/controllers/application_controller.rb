9c1bed3d79ab836ee5096a64911aa4c464f04179
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-02-02 12:57:06 +0000

    [add] 商品画面レイアウト(30%)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :admin
+        admin_root_path
+      else
+        root_path
+      end
+  end
 end
