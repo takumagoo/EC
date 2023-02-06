cb4b96866fe82b98d2937f490dd2ed95fc4bff4d
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-02-06 06:56:38 +0000

    [add] 配送先画面(80%)

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,14 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def complete
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+   # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
   namespace :admin do
     resources :orders, only: [:show, :update]
@@ -14,35 +26,32 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
-  
+
   namespace :admin do
     # get 'genres/index'
     # get 'genres/edit'
     resources :genres, only: [:index, :create, :edit, :update]
   end
-  
+
   # 顧客
-  namespace :public do
+  scope module: :public do
     resources :items, only: [:index, :show]
     root to: "homes#top"
     get 'homes/about' => 'homes#about'
-  end
-  namespace :public do
-    resources :customers, only: [:edit, :update, :unsubscribe, :withdrawal, :destroy]
-    get '/customers/my_page' => 'coustomers#show'
-  end
-  
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
 
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
+    resources :customers, only: [:edit, :update, :unsubscribe, :withdrawal, :destroy]
+    get '/customers/my_page' => 'customers#show'
+
+    resources :orders, only: [:new, :index, :show, :create, :confirm]
+    get 'orders/complete' => 'orders#complete'
+
+    resources :cart_items, only: [:index, :create, :update, :destroy, :destroy_all]
+
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
