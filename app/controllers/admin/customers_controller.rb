5eda768570783ea9b07a531bae2bc249077df1db
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-02-24 06:00:50 +0000

    [add] 管理者画面 95% 完

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,18 +11,18 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     customer = Customer.find(params[:id])
     customer.update(customer_params)
     redirect_to admin_customers_path
   end
-  
+
   private
 
    # ストロングパラメータ
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :email, :genre_id, :price, :is_deleted)
   end
-  
+
 end
